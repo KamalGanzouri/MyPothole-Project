ba93b9d3e5f3fd2d5f3f8a89764f6163c99e8923
KamalGanzouri <kamalabdulhalimganzouri@gmail.com>
2023-03-08 21:51:04 +0200

    Update main.py, update requirements.txt Add Connection to firebase 'firestone' database Add /location Add /location/bad Add /location/dangerous Add Lat and Long to detect function

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from fastapi import FastAPI, UploadFile, HTTPException
 
 from ultralytics import YOLO
 
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+database = firestore.client()
+
 model = YOLO('Model.pt')
 
 app = FastAPI()
@@ -17,8 +24,10 @@ async def root():
 
 
 @app.post("/detect")
-async def detect(img: UploadFile):
-    massage = ""
+async def detect(img: UploadFile, lat: float, long: float):
+    user_id = "id"
+    location = firestore.GeoPoint(lat, long)
+    admin_id = "id"
     if img.filename.split(".")[-1] in ("jpg", "jpeg", "png"):
         pass
     else:
@@ -26,7 +35,50 @@ async def detect(img: UploadFile):
     image = Image.open(BytesIO(img.file.read()))
     image = np.array(image)
     result = model(image)
+    pothole_type = ""
     for r in result:
         for c in r.boxes.cls:
-            massage = model.names[int(c)]
-    return {"message": massage}
+            pothole_type = model.names[int(c)]
+            if pothole_type == "Dangerous":
+                break
+    if pothole_type in ("Bad", "Dangerous"):
+        database.collection('pothole').add({'user_id': user_id, 'type': pothole_type, 'location': location,
+                                            'fixed': False, 'admin_id': admin_id})
+        return 'Detected'
+    else:
+        return 'No Detection'
+
+
+@app.get("/location")
+async def locations():
+    docs = database.collection('pothole').where("fixed", "==", False).stream()
+    location = []
+    for doc in docs:
+        doc = doc.to_dict()
+
+        location_and_type = {'latitude': doc.get('location').latitude, 'longitude': doc.get('location').longitude,
+                             'type': doc.get('type')}
+        location.append(location_and_type)
+    return location
+
+
+@app.get("/location/bad")
+async def bad_locations():
+    docs = database.collection("pothole").where("fixed", "==", False).where("type", "==", "Bad").stream()
+    location = []
+    for doc in docs:
+        doc = doc.to_dict()
+        location_only = {'latitude': doc.get('location').latitude, 'longitude': doc.get('location').longitude}
+        location.append(location_only)
+    return location
+
+
+@app.get("/location/dangerous")
+async def dangerous_locations():
+    docs = database.collection("pothole").where("fixed", "==", False).where("type", "==", "Dangerous").stream()
+    location = []
+    for doc in docs:
+        doc = doc.to_dict()
+        location_only = {'latitude': doc.get('location').latitude, 'longitude': doc.get('location').longitude}
+        location.append(location_only)
+    return location
