7657d5b9cc4d01497d06729558faf22ca493b7ff
kamal <kamalabdulhalimganzouri@gmail.com>
2023-03-09 03:06:01 +0200

    Try 2

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ async def detect(img: UploadFile, lat: float, long: float):
         pass
     else:
         raise HTTPException(status_code=415, detail="wrong format")'''
-    '''image = Image.open(BytesIO(img.file.read()))
-    image = np.array(image)'''
-    result = model(img)
+    image = Image.open(BytesIO(img.file.read()))
+    image = np.array(image)
+    result = model(image)
     pothole_type = ""
     for r in result:
         for c in r.boxes.cls:
