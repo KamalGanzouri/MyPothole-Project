52e5c3c6b991921f56af74209067d8e0cf484af5
ganzouri <kamalabdulhalimganzouri@gmail.com>
2023-03-13 19:12:56 +0200

    Update : 13/3

--- firebase.py
+++ firebase.py
@@ -1,19 +1,5 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from ultralytics import YOLO
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-database = firestore.client()
-location = firestore.GeoPoint(90, 90)
+model = YOLO("C:/Users/kamal/Downloads/best.pt")
 
-docs = database.collection('pothole').stream()
-print(docs)
-
-location = []
-
-for doc in docs:
-    doc = doc.to_dict()
-    location.append(([doc.get('location').latitude, doc.get('location').longitude], doc.get('type')))
-
-print(location)
+model.export(format="tflite")
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from io import BytesIO
 
-import firebase_admin
 import numpy as np
 from PIL import Image
 from fastapi import FastAPI, UploadFile
+import firebase_admin
 from firebase_admin import credentials, firestore
 from ultralytics import YOLO
 
@@ -23,25 +23,32 @@ async def root():
 
 @app.post("/detect")
 async def detect(img: UploadFile, lat: float, long: float):
-    user_id = "id"
+
     location = firestore.GeoPoint(lat, long)
-    admin_id = "id"
+    docs = database.collection('pothole').where("location", "==", location).get()
+    if docs:
+        return "Already Saved"
+
     '''if img.filename.split(".")[-1] in ("jpg", "jpeg", "png"):
         pass
     else:
         raise HTTPException(status_code=415, detail="wrong format")'''
+
     image = Image.open(BytesIO(img.file.read()))
     image = np.array(image)
     result = model(image)
+
     pothole_type = ""
+
     for r in result:
         for c in r.boxes.cls:
             pothole_type = model.names[int(c)]
             if pothole_type == "Dangerous":
                 break
+
     if pothole_type in ("Bad", "Dangerous"):
-        database.collection('pothole').add({'user_id': user_id, 'type': pothole_type, 'location': location,
-                                            'fixed': False, 'admin_id': admin_id})
+        database.collection('pothole').add({'user_id': "NotDefinedYet", 'type': pothole_type, 'location': location,
+                                            'fixed': False, 'employee_id': "NotDefinedYet"})
         return 'Detected'
     else:
         return 'No Detection'
@@ -80,3 +87,12 @@ async def dangerous_locations():
         location_only = {'latitude': doc.get('location').latitude, 'longitude': doc.get('location').longitude}
         location.append(location_only)
     return location
+
+
+@app.put("/pothole-fixed")
+async def pothole_fix(lat: float, long: float, employee_id: str):
+    location = firestore.GeoPoint(lat, long)
+    docs = database.collection('pothole').where("location", "==", location).get()
+    for doc in docs:
+        database.collection('pothole').document(doc.id).update({'fixed': True, 'employee_id': employee_id})
+    return "Pothole marked as fixed"
